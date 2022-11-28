d5cfefe6560b2b468d97097e171ff5be7d72a726
Chukwubuikem Chukwudi <myecodevelopment@gmail.com>
2022-11-28 18:13:04 +0100

    Initial Commit

---
+++ Contacts List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts List
+//
+//  Created by Eco Dev System on 28/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
